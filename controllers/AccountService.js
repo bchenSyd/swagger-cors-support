240fa331abc04814ca519c3a1ee00b5b785cab1a
Bo Chen <bochen2014@yahoo.com>
2017-06-27 09:49:28 +0000

    modernize

--- controllers/AccountService.js
+++ controllers/AccountService.js
@@ -1,6 +1,8 @@
 'use strict';
 
 exports.login = function (args, res, next) {
+  const { code:{value: auth_code}} = args;
+  console.log(auth_code);
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({
     AccessToken: 'access_token'
